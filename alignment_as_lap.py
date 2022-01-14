ea92777546b0b229dbf89eeb8dd1506945f11c32
ele <Emanuele Olivetti>
2022-01-14 19:56:13 +0100

    Initial import

---
+++ alignment_as_lap.py
@@ -0,0 +1,313 @@
+"""Alignment of tractograms as LAP.
+
+Copyright Emanuele Olivetti, 2018, 2020, 2022
+BSD License, 3 clauses.
+
+"""
+
+import numpy as np
+from nibabel.streamlines import load
+from dissimilarity import compute_dissimilarity, dissimilarity
+from kmeans import mini_batch_kmeans, compute_labels, compute_centroids
+from scipy.spatial import KDTree
+from dipy.tracking.streamline import set_number_of_points
+from dipy.tracking.distances import bundles_distances_mam, bundles_distances_mdf
+from dipy.tracking.streamlinespeed import length
+from scipy.optimize import linear_sum_assignment
+
+
+try:
+    from joblib import Parallel, delayed
+    joblib_available = True
+except ImportError:
+    joblib_available = False
+
+
+def load_tractogram(T_filename, threshold_short_streamlines=10.0, nb_points=16):
+    """Load tractogram from TRK file, resample to the desired number of
+    points and remove short streamlines with length below threshold.
+    """
+    print("Loading %s" % T_filename)
+    data = load(T_filename)
+    T = data.streamlines
+    print("%s: %s streamlines" % (T_filename, len(T)))
+
+    if nb_points is not None:
+        print("Resampling all streamlines to %s points" % (nb_points,))
+        T = set_number_of_points(T, nb_points)
+
+    T = np.array(T, dtype=object)
+    
+    if threshold_short_streamlines > 0:
+        # Removing short artifactual streamlines
+        print("Removing (presumably artifactual) streamlines shorter than %s" % threshold_short_streamlines)
+        T = np.array([s for s in T if length(s) >= threshold_short_streamlines], dtype=object)
+        print("%s: %s streamlines" % (T_filename, len(T)))
+
+    return T
+
+
+def clustering(S_dr, k, b=100, t=100):
+    """Wrapper of the mini-batch k-means algorithm that combines multiple
+    basic functions into a convenient one.
+    """
+    # Generate k random centers:
+    S_centers = S_dr[np.random.permutation(S_dr.shape[0])[:k]]
+
+    # Improve the k centers with mini_batch_kmeans
+    S_centers = mini_batch_kmeans(S_dr, S_centers, b=b, t=t)
+
+    # Assign the cluster labels to each streamline. The label is the
+    # index of the nearest center.
+    S_cluster_labels = compute_labels(S_dr, S_centers)
+
+    # Compute a cluster representive, for each cluster
+    S_representatives_idx = compute_centroids(S_dr, S_centers)
+    return S_representatives_idx, S_cluster_labels
+
+
+def LAP(S_A, S_B, verbose=True, parallel=True, distance_function=bundles_distances_mam):
+    """
+    """
+    assert(len(S_B) >= len(S_A))  # required by LAP
+    if verbose:
+        print("Computing LAP between streamlines.")
+        print("Computing the distance matrix between the two sets of streamlines.")
+
+    dm_AB = streamline_distance(S_A, S_B, parallel=parallel,
+                                distance_function=distance_function)
+    _, corresponding_streamlines = linear_sum_assignment(dm_AB)
+    return corresponding_streamlines
+
+
+def streamline_distance(S_A, S_B=None, parallel=True,
+                        distance_function=bundles_distances_mam):
+    """Wrapper to decide what streamline distance function to use. The
+    function computes the distance matrix between sets of
+    streamlines. This implementation provides optimiztions like
+    parallelization and avoiding useless computations when S_B is
+    None.
+    """
+    # distance_function = bundles_distances_mdf
+    if parallel:
+        return dissimilarity(S_A, S_B, distance_function)
+    else:
+        return distance_function(S_A, S_B)
+
+
+def distance_corresponding(A, B, correspondence,
+                           distance_function=bundles_distances_mam):
+    """Distance between streamlines in set A and the corresponding ones in
+    B. The vector 'correspondence' has in position 'i' the index of
+    the streamline in B that corresponds to A[i].
+
+    """
+    return np.array([streamline_distance([A[i]],
+                                         [B[correspondence[i]]],
+                                         parallel=False,
+                                         distance_function=distance_function)
+                     for i in range(len(A))]).squeeze()
+
+
+def LAP_two_clusters(cluster_A, cluster_B, alpha=0.5, max_iter1=100,
+                     max_iter2=100, parallel=True,
+                     distance_function=bundles_distances_mam):
+    """Wrapper of LAP() between the streamlines of two clusters. This code
+    is able two handle clusters of different sizes and to invert the
+    result of corresponding_streamlines, if necessary.
+    """
+    if len(cluster_A) <= len(cluster_B):  # LAP(A,B)
+        corresponding_streamlines = LAP(cluster_A, cluster_B,
+                                        verbose=False,
+                                        parallel=parallel,
+                                        distance_function=distance_function)
+    else:  # LAP(B,A)
+        corresponding_streamlines = LAP(cluster_B, cluster_A,
+                                        verbose=False,
+                                        parallel=parallel,
+                                        distance_function=distance_function)
+        # invert result from B->A to A->B:
+        tmp = -np.ones(len(cluster_A), dtype=int)
+        for j, v in enumerate(corresponding_streamlines):
+            if v != -1:
+                tmp[v] = j
+
+        corresponding_streamlines = tmp
+
+    return corresponding_streamlines
+
+
+def LAP_all_corresponding_pairs(T_A, T_B, k,
+                                T_A_cluster_labels,
+                                T_B_cluster_labels,
+                                corresponding_clusters,
+                                distance_function=bundles_distances_mam):
+    """Loop over all pairs of correponding clusters and perform graph
+    matching between the streamlines of correponding clusters.
+
+    This code executes parallel (multicore) for-loop, if joblib is
+    available. If not, it reverts to a standard for-loop.
+    """
+    print("Compute LAP between streamlines of corresponding clusters")
+    correspondence_lap = -np.ones(len(T_A), dtype=int)  # container of the results
+    if joblib_available:
+        print("Parallel version: executing %s tasks in parallel" % k)
+        n_jobs = -1
+        clusters_A_idx = [np.where(T_A_cluster_labels == i)[0] for i in range(k)]
+        clusters_A = [T_A[clA_idx] for clA_idx in clusters_A_idx]
+        clusters_B_idx = [np.where(T_B_cluster_labels == corresponding_clusters[i])[0] for i in range(k)]
+        clusters_B = [T_B[clB_idx] for clB_idx in clusters_B_idx]
+        results = Parallel(n_jobs=n_jobs, verbose=True)(delayed(LAP_two_clusters)(clusters_A[i], clusters_B[i], parallel=False, distance_function=distance_function) for i in range(k))
+        # merge results
+        for i in range(k):
+            tmp = results[i] != -1
+            correspondence_lap[clusters_A_idx[i][tmp]] = clusters_B_idx[i][results[i][tmp]]
+
+    else:
+        for i in range(k):
+            print("LAP between streamlines of corresponding clusters: cl_A=%s <-> cl_B=%s" % (i, corresponding_clusters[i]))
+            cluster_A_idx = np.where(T_A_cluster_labels == i)[0]
+            cluster_A = T_A[cluster_A_idx]
+            cluster_B_idx = np.where(T_B_cluster_labels == corresponding_clusters[i])[0]
+            cluster_B = T_B[cluster_B_idx]
+            corresponding_streamlines = LAP_two_clusters(cluster_A,
+                                                         cluster_B,
+                                                         distance_function=distance_function)
+
+            tmp = corresponding_streamlines != -1
+            correspondence_lap[cluster_A_idx[tmp]] = cluster_B_idx[corresponding_streamlines[tmp]]
+
+    return correspondence_lap
+
+
+def fill_missing_correspondences(correspondence_lap, T_A_dr):
+    """After LAP, in case some correspondences are missing,
+    i.e. their target index is '-1', fill them following this idea:
+    for a given streamline T_A[i], its correponding one in T_B is the
+    one corresponding to the nearest neighbour of T_A[i] in T_A.
+
+    The (approximate nearest neighbour) is computed with a KDTree on
+    the dissimilarity representation of T_A, i.e. T_A_dr.
+    """
+    print("Filling missing correspondences in T_A with the corresponding to their nearest neighbour in T_A")
+    correspondence = correspondence_lap.copy()
+    T_A_corresponding_idx = np.where(correspondence != -1)[0]
+    T_A_missing_idx = np.where(correspondence == -1)[0]
+    T_A_corresponding_kdt = KDTree(T_A_dr[T_A_corresponding_idx])
+    # T_A_missing_NN = T_A_corresponding_kdt.query(T_A_dr[T_A_missing_idx],
+    #                                              k=1,
+    #                                              return_distance=False,
+    #                                              workers=-1).squeeze()
+    _, T_A_missing_NN = T_A_corresponding_kdt.query(T_A_dr[T_A_missing_idx],
+                                                    k=1,
+                                                    workers=-1)
+    correspondence[T_A_missing_idx] = correspondence[T_A_corresponding_idx[T_A_missing_NN]]
+    return correspondence
+
+
+def alignment_as_LAP(T_A, T_B,
+                     k, threshold_short_streamlines=10.0,
+                     b=100,
+                     t=100,
+                     T_A_dr=None,  # precomputed dissimilarity representation, useful for large experiments
+                     T_B_dr=None,
+                     distance_function=bundles_distances_mam):
+
+    # 2) Compute the dissimilarity representation of T_A and T_B
+    if T_A_dr is None:
+        print("Computing the dissimilarity representation of T_A")
+        T_A_dr, prototypes_A = compute_dissimilarity(T_A, distance=distance_function)
+
+    if T_B_dr is None:
+        print("Computing the dissimilarity representation of T_B")
+        T_B_dr, prototypes_B = compute_dissimilarity(T_B, distance=distance_function)
+
+    # 3) Compute the k-means clustering of T_A and T_B
+    b = 100  # mini-batch size
+    t = 100  # number of iterations
+    print("Computing the k-means clustering of T_A and T_B, k=%s" % k)
+    print("mini-batch k-means on T_A")
+    T_A_representatives_idx, T_A_cluster_labels = clustering(T_A_dr, k=k, b=b, t=t)
+    print("mini-batch k-means on T_B")
+    T_B_representatives_idx, T_B_cluster_labels = clustering(T_B_dr, k=k, b=b, t=t)
+
+    # 4) Compute LAP between T_A_representatives and T_B_representatives
+    alpha = 0.5
+    max_iter1 = 100
+    max_iter2 = 100
+    corresponding_clusters = LAP(T_A[T_A_representatives_idx],
+                                 T_B[T_B_representatives_idx],
+                                 distance_function=distance_function)
+    distance_clusters = distance_corresponding(T_A[T_A_representatives_idx],
+                                               T_B[T_B_representatives_idx],
+                                               corresponding_clusters,
+                                               distance_function=distance_function)
+    print("Median distance between corresponding clusters: %s" % np.median(distance_clusters))
+
+    # 5) For each pair corresponding cluster, compute LAP
+    # between their streamlines
+    correspondence_lap = LAP_all_corresponding_pairs(T_A, T_B, k,
+                                                     T_A_cluster_labels,
+                                                     T_B_cluster_labels,
+                                                     corresponding_clusters,
+                                                     distance_function=distance_function)
+
+    # 6) Filling the missing correspondences in T_A with the
+    # correspondences of the nearest neighbors in T_A
+    correspondence = fill_missing_correspondences(correspondence_lap, T_A_dr)
+
+    # 7) Compute the mean distance of corresponding streamlines, to
+    # check the quality of the result
+    distances = distance_corresponding(T_A, T_B, correspondence,
+                                       distance_function=distance_function)
+    print("Median distance of corresponding streamlines: %s" % np.median(distances))
+
+    return correspondence, distances
+
+
+if __name__ == '__main__':
+    print(__doc__)
+    np.random.seed(0)
+
+    T_A_filename = 'data/sub-500222/dt-neuro-track-trk.tag-ensemble.tag-t1.id-605a1e1c73b69eaef86d2f54/track.trk'
+    T_B_filename = 'data/sub-506234/dt-neuro-track-trk.tag-ensemble.tag-t1.id-605a1ee373b69e81bf6d34f9/track.trk'
+
+    distance_function = bundles_distances_mdf
+    
+    # Main parameters:
+    k = 1000  # number of clusters, usually somewhat above sqrt(|T_A|) is optimal for efficiency.
+    threshold_short_streamlines = 0.0  # Beware: discarding streamlines affects IDs
+
+    # Additional internal parameters for mini-batch k-means, no need to change them, usually:
+    b = 100
+    t = 100
+
+    # Number of points to which resample each streamline, for performance reasons:
+    nb_points = 32
+
+    # 1) load T_A and T_B
+    T_moving = load_tractogram(T_A_filename,
+                               threshold_short_streamlines=threshold_short_streamlines,
+                               nb_points=nb_points)
+    T_static = load_tractogram(T_B_filename,
+                               threshold_short_streamlines=threshold_short_streamlines,
+                               nb_points=nb_points)
+
+    correspondence, distances = alignment_as_LAP(T_A=T_moving,
+                                                 T_B=T_static,
+                                                 k=k,
+                                                 threshold_short_streamlines=threshold_short_streamlines,
+                                                 b=b,
+                                                 t=t,
+                                                 distance_function=distance_function)
+
+    print("Saving the result into correspondence.csv")
+    result = np.vstack([range(len(correspondence)), correspondence]).T
+    np.savetxt("correspondence.csv", result, fmt='%d', delimiter=',',
+               header='ID_A,ID_B')
+
+    import matplotlib.pyplot as plt
+    plt.interactive(True)
+    plt.figure()
+    plt.hist(distances, bins=50)
+    plt.title("Distances between corresponding streamlines")
---
+++ kmeans.py
@@ -0,0 +1,125 @@
+import numpy as np
+from scipy.spatial import KDTree
+
+
+def kmeans(X, C):
+    """The Loyd's algorithm for the k-centers problems.
+
+    X : data matrix
+    C : initial centers
+    """
+    C = C.copy()
+    V = np.zeros(C.shape[0])
+    for x in X:
+        idx = np.argmin(((C - x)**2).sum(1))
+        V[idx] += 1
+        eta = 1.0 / V[idx]
+        C[idx] = (1.0 - eta) * C[idx] + eta * x
+
+    return C
+
+
+def mini_batch_kmeans(X, C, b, t, replacement=True):
+    """The mini-batch k-means algorithms (Sculley et al. 2007) for the
+    k-centers problem.
+
+    X : data matrix
+    C : initial centers
+    b : size of the mini-batches
+    t : number of iterations
+    replacement: whether to sample batches with replacement or not.
+    """
+    C = C.copy()
+    for i in range(t):
+        # Sample a mini batch:
+        if replacement:
+            X_batch = X[np.random.permutation(X.shape[0])[:b]]
+        else:
+            X_batch = X[b*t:b*(t+1)]
+
+        V = np.zeros(C.shape[0])
+        idxs = np.empty(X_batch.shape[0], dtype=int)
+        # Assign the closest centers without update for the whole batch:
+        for j, x in enumerate(X_batch):
+            idxs[j] = np.argmin(((C - x)**2).sum(1))
+
+        # Update centers:
+        for j, x in enumerate(X_batch):
+            V[idxs[j]] += 1
+            eta = 1.0 / V[idxs[j]]
+            C[idxs[j]] = (1.0 - eta) * C[idxs[j]] + eta * x
+
+    return C
+
+
+def compute_labels(X, C):
+    """Compute the cluster labels for dataset X given centers C.
+    """
+    # labels = np.argmin(pairwise_distances(C, X), axis=0) # THIS REQUIRES TOO MUCH MEMORY FOR LARGE X
+    tree = KDTree(C)
+    _, labels = tree.query(X, k=1, workers=-1)
+    return labels
+
+
+def compute_centroids(X, C):
+    """Compute the centroids for dataset X given centers C. Note: centers
+    C may not belong to X.
+    """
+    tree = KDTree(X)
+    _, centroids = tree.query(C, k=1, workers=-1)
+    return centroids
+
+
+if __name__ == '__main__':
+
+    from sklearn.datasets import make_blobs
+    import matplotlib.pyplot as plt
+    from sklearn.metrics import adjusted_rand_score
+
+    np.random.seed(1)
+
+    n = 10000
+    d = 2
+    X, y = make_blobs(n, d, centers=3)
+    plt.plot(X[:,0], X[:,1], 'ko')
+
+    k = 3
+
+    # In case we want to permute the order of X:
+    # np.random.seed(0)
+    # X = np.random.permutation(X)
+
+    C_init = X[:k]
+    plt.plot(C_init[:,0], C_init[:,1], 'bo', markersize=10, label='initialization')
+
+    C_kmeans = kmeans(X, C_init)
+
+    plt.plot(C_kmeans[:,0], C_kmeans[:,1], 'ro', markersize=10, label='k-means')
+
+    b = 50
+    t = 10
+
+    C_mbkm = mini_batch_kmeans(X, C_init, b=b, t=t, replacement=True)
+    plt.plot(C_mbkm[:,0], C_mbkm[:,1], 'go', markersize=10, label='mini-batch k-means')
+
+    C_mbkm_wr = mini_batch_kmeans(X, C_init, b=b, t=t, replacement=False)
+    plt.plot(C_mbkm_wr[:,0], C_mbkm_wr[:,1], 'mo', markersize=10, label='mini-batch k-means w/o rep.')
+
+    # from sklearn.cluster import MiniBatchKMeans
+    # mbkm_skl = MiniBatchKMeans(n_clusters=k, max_iter=1, max_no_improvement=None, tol=0.0, batch_size=b, init=C_init, compute_labels=False)
+    # mbkm_skl.fit(X)
+    # C_mbkm_skl = mbkm_skl.cluster_centers_
+    # plt.plot(C_mbkm_skl[:,0], C_mbkm_skl[:,1], 'co', markersize=10, label='mini-batch k-means SKL')
+
+    plt.legend(numpoints=1, loc='lower right')
+
+    labels_init = compute_labels(X, C_init)
+    labels_kmeans = compute_labels(X, C_kmeans)
+    labels_mbkm = compute_labels(X, C_mbkm)
+    labels_mbkm_wr = compute_labels(X, C_mbkm_wr)
+    print("Adjusted rand scores:")
+    print("labels_kmeans, labels_init =", adjusted_rand_score(labels_kmeans, labels_init))
+    print("labels_kmeans, labels_mbkm =", adjusted_rand_score(labels_kmeans, labels_mbkm))
+    print("labels_kmeans, labels_mbkm_wr =", adjusted_rand_score(labels_kmeans, labels_mbkm_wr))
+
+    plt.show()
